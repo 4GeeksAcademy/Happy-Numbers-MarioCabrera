86a4da8c8e4385d0c599044db7eb8ed16247d213
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-06-27 11:31:36 +0000

    Happy Numbers

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+document.getElementById("start").addEventListener("click", () => {
+  const numero = prompt("¿Es tu número feliz?");
+  let result = ""
+  const historial = new Set();
+let registro = []
+
+function esFeliz(n) {
+  let num = parseInt(n);
+
+  while (num !== 1 && !historial.has(num)) {
+    historial.add(num);
+    registro.push(num)
+    num = sumaCuadrados(num);
+  }
+
+  return num === 1;
+}
+
+function sumaCuadrados(num) {
+  return num
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((acc, digit) => acc + digit * digit, 0);
+}
+  if (esFeliz(numero)) {
+    result = "Happy! 🎉"
+  } else {
+    result = "Unhappy 😢"
+  }
+  document.getElementById("myNumber").innerText = numero
+  document.getElementById("result").innerText = result
+  document.getElementById("historial").innerHTML = registro
+  
+})
+
+
+
+// var digits = numbers.toString().split('');
+// var realDigits = digits.map(Number)
+// console.log(realDigits);
